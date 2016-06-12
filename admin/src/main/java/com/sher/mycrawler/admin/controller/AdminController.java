3cef213f600569fbab0d10b8bde3ffa90787b18c
zhaowei <zhaowei@dasmaster.com>
2016-06-13 07:52:40 +0800

    add controller

--- admin/src/main/java/com/sher/mycrawler/admin/controller/AdminController.java
+++ admin/src/main/java/com/sher/mycrawler/admin/controller/AdminController.java
@@ -1,10 +1,31 @@
 package com.sher.mycrawler.admin.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by sher on 6/6/16.
  *
  * 后台管理
  */
 
+@RestController
+@EnableAutoConfiguration
 public class AdminController {
+
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+
+        return "hello world";
+    }
+
+
+
+
+
 }
